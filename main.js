425faa941639cbdebb847c7ffe8fac7eda264375
Arek <wojcikarek10@gmail.com>
2022-05-08 15:31:21 +0200

    add THREE

--- main.js
+++ main.js
@@ -1,6 +1,34 @@
 import './style.css'
+import * as THREE from 'three';
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000)
+
+const renderer = new THREE.WebGLRenderer({
+
+  canvas: document.querySelector('#bg')
+
+});
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+
+function onWindowResize(){
+
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+
+function animate() {
+
+  requestAnimationFrame(animate);
+  renderer.render(scene,camera);
+
+}
+
+animate();
+
+window.addEventListener( 'resize', onWindowResize, false );
